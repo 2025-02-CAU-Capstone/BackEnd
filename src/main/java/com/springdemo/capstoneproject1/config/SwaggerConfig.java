746f12b2e7e6e9a049ff161376489b9f3609f7bf
Mingyu Lee <mingyu.lee0327@gmail.com>
2025-11-18 17:49:20 +0900

    added swaggerconfig and corsconfig (corsconfig is for later usage; remove if this causes any issue) #15

---
+++ src/main/java/com/springdemo/capstoneproject1/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.springdemo.capstoneproject1.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://13-209-30-220.nip.io").description("HTTPS Server")
+                ));
+    }
+}
