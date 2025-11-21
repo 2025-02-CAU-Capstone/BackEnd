eb214d51ec25dd9bc64fb43673e4677ff63d2ac8
Mingyu Lee <mingyu.lee0327@gmail.com>
2025-11-22 02:54:31 +0900

    fixed typo #26

--- src/main/java/com/springdemo/capstoneproject1/dto/ChapterDTO.java
+++ src/main/java/com/springdemo/capstoneproject1/dto/ChapterDTO.java
@@ -8,5 +8,7 @@ public class ChapterDTO {
     private Integer chapterId;
     private String title;
     private Integer orderIndex;
+    private String url;
+    private Double duration;
     private LocalDateTime createdAt;
 }
