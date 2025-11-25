484860a38ab58121b5d01ab7853ff7cd45d65640
Mingyu Lee <mingyu.lee0327@gmail.com>
2025-11-25 15:31:11 +0900

    feat: edited OCR to accept multipart #48

--- src/main/java/com/springdemo/capstoneproject1/client/AIOCRClient.java
+++ src/main/java/com/springdemo/capstoneproject1/client/AIOCRClient.java
@@ -3,26 +3,34 @@ package com.springdemo.capstoneproject1.client;
 import com.springdemo.capstoneproject1.dto.OCRResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 @RequiredArgsConstructor
 public class AIOCRClient {
 
-    private final WebClient aiWebClient; // baseUrl 설정된 WebClient
+    private final WebClient aiWebClient;
 
     public OCRResponse ocr(MultipartFile image) {
+        try {
+            MultipartBodyBuilder builder = new MultipartBodyBuilder();
+            builder.part("file", image.getResource())
+                    .filename(image.getOriginalFilename())
+                    .contentType(MediaType.parseMediaType(image.getContentType()));
 
-        return aiWebClient.post()
-                .uri("/ocr")   // FastAPI OCR 엔드포인트
-                .contentType(MediaType.MULTIPART_FORM_DATA)
-                .body(BodyInserters
-                        .fromMultipartData("file", image.getResource()))
-                .retrieve()
-                .bodyToMono(OCRResponse.class)
-                .block();   // 동기 호출
+            return aiWebClient.post()
+                    .uri("/ocr")
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .bodyValue(builder.build())
+                    .retrieve()
+                    .bodyToMono(OCRResponse.class)
+                    .block();
+
+        } catch (Exception e) {
+            throw new RuntimeException("FastAPI OCR 요청 실패: " + e.getMessage(), e);
+        }
     }
 }
